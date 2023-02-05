f69d2c374cd3eb9f684c3d792b30ad5556f5fc4f
AlexBahamaz <123762590+AlexBahamaz@users.noreply.github.com>
2023-02-05 21:15:22 +0300

    Add files via upload

---
+++ car_efficiency.py
@@ -0,0 +1,8 @@
+import random
+
+def car_efficiency(weight, power): #функция расчёта удельной мощности авто
+    return weight/power
+
+cars_data = [[random.randint(50,500) for _ in range(10)] for j in range(2)] #заполняем двухмерный массив со случайными данными первая строка масса, вторая мощность
+
+print([car_efficiency(cars_data[0][i], cars_data[1][i]) for i in range(len(cars_data[0]))])
