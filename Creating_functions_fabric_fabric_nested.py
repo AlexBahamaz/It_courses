8a0d19e26be889a5305f45219984e47e4aa652ed
AlexBahamaz <123762590+AlexBahamaz@users.noreply.github.com>
2023-02-05 20:12:38 +0300

    Add files via upload

---
+++ Creating_functions_fabric_fabric_nested.py
@@ -0,0 +1,16 @@
+#вложенный метод создания функции
+def decrease_times_nested(times):
+    def input_list():
+        return [float(i)/times for i in input().split()]
+    return input_list()
+
+#фабричный подход создания функции
+def decrease_times_fabric(times):
+    def input_list():
+        return [float(i)/times for i in input().split()]
+    return input_list
+
+print(decrease_times_nested(5)) #печать с помощью вложенной функции
+
+decrease_five_input_list = decrease_times_fabric(5)
+print(decrease_five_input_list()) #печать с помощью функции с фабричным подходом
